b28d7ef005f1f30a5b8018749b5425580d2200ac
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-03-13 16:39:00 +0500

    3: add addTaskButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import { AddTaskButton } from "./components/buttons/AddTaskButton";
 import { TasksBlock } from "./components/tasksBlock/TasksBlock";
 
 function App() {
 	return (
 		<div className="App">
+            <div className="AppHeader">
+					<AddTaskButton />
+				</div>
 			<TasksBlock />
 		</div>
 	);
---
+++ src/components/buttons/AddTaskButton.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Button, Dropdown, DropdownButton, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useAppDispatch } from "../../store";
+import { TasksActions } from "../../store/tasks";
+
+import "./AddTaskButton.css";
+
+export function AddTaskButton() {
+	const dispatch = useAppDispatch();
+	const [value, setValue] = useState("");
+    const [valueCategory, setValueCategory] = useState("all")
+
+	const AddTask = () => {
+		dispatch(TasksActions.addTask({ title: value, category: valueCategory }));
+		setValue("");
+	};
+
+    const changeCategory = (e: any) => {
+        setValueCategory(e.target.value)
+    }
+
+	return (
+		<div className="addTask__container">
+			<Form.Control
+				value={value}
+				onChange={(event) => {
+					setValue(event.target.value);
+				}}
+				type="text"
+				placeholder="Введите задачу"
+				className="input"
+			/>
+			<Form.Select onChange={(e: any) => {changeCategory(e)}} className="category" aria-label="Default select example">
+				<option>Выберите категорию</option>
+				<option value="all">Все</option>
+				<option value="home">Домашние</option>
+				<option value="work">Рабочие</option>
+			</Form.Select>
+			<Button
+				className="addTask__btn"
+				onClick={AddTask}
+				variant="outline-secondary"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					className="bi bi-plus-lg"
+					viewBox="0 0 16 16"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+					/>
+				</svg>
+			</Button>
+		</div>
+	);
+}
